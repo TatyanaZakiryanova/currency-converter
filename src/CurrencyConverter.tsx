666e760180595d22830ad7be5848ae073e7450d2
TatyanaZakiryanova <elenastrell@yandex.ru>
2024-12-07 18:58:24 +0300

    getting exchange rates via exchangerate api

--- src/CurrencyConverter.tsx
+++ src/CurrencyConverter.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  CircularProgress,
   FormControl,
   InputLabel,
   MenuItem,
@@ -8,80 +9,103 @@ import {
   TextField,
   Typography,
 } from '@mui/material';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 
-type Currency = 'USD' | 'EUR' | 'RUB';
-
-const exchangeRates: Record<Currency, number> = {
-  USD: 1.06,
-  EUR: 1,
-  RUB: 100,
-};
+type Rates = Record<string, number>;
 
 const CurrencyConverter = () => {
   const [amount, setAmount] = useState<number>(0);
-  const [fromCurrency, setFromCurrency] = useState<Currency>('USD');
-  const [toCurrency, setToCurrency] = useState<Currency>('EUR');
+  const [rates, setRates] = useState<Rates>({});
+  const [fromCurrency, setFromCurrency] = useState<string>('USD');
+  const [toCurrency, setToCurrency] = useState<string>('EUR');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'https://v6.exchangerate-api.com/v6/460b6f3df688ca4bbb39ac9e/latest/USD',
+        );
+        const data = await response.json();
+        setRates(data.conversion_rates);
+      } catch {
+        console.error('API Error');
+      }
+    };
+    fetchData();
+  }, []);
 
   const handleAmountInput = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     if (value === '' || !isNaN(Number(value))) setAmount(value === '' ? 0 : Number(value));
   };
 
-  const handleFromCurrencySelect = (e: SelectChangeEvent<'USD' | 'EUR' | 'RUB'>) => {
-    setFromCurrency(e.target.value as Currency);
+  const handleFromCurrencySelect = (e: SelectChangeEvent<string>) => {
+    setFromCurrency(e.target.value);
   };
 
-  const handleToCurrencySelect = (e: SelectChangeEvent<'USD' | 'EUR' | 'RUB'>) => {
-    setToCurrency(e.target.value as Currency);
+  const handleToCurrencySelect = (e: SelectChangeEvent<string>) => {
+    setToCurrency(e.target.value);
   };
 
-  const convertedAmount = (amount / exchangeRates[fromCurrency]) * exchangeRates[toCurrency];
+  const convertedAmount =
+    rates[fromCurrency] && rates[toCurrency]
+      ? (amount / rates[fromCurrency]) * rates[toCurrency]
+      : 0;
   const validConvertedAmount = Math.max(0, convertedAmount);
 
   return (
     <Box>
-      <TextField
-        label="Amount"
-        type="number"
-        value={amount === 0 ? '' : amount}
-        onChange={handleAmountInput}
-        placeholder="Enter value..."
-        sx={{ marginBottom: 3, width: '100%' }}
-      />
-      <FormControl fullWidth sx={{ marginBottom: 2 }}>
-        <InputLabel>From</InputLabel>
-        <Select value={fromCurrency} onChange={handleFromCurrencySelect} label="From">
-          {Object.keys(exchangeRates).map((currency) => (
-            <MenuItem key={currency} value={currency}>
-              {currency}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <FormControl fullWidth sx={{ marginBottom: 2 }}>
-        <InputLabel>To</InputLabel>
-        <Select value={toCurrency} onChange={handleToCurrencySelect} label="To">
-          {Object.keys(exchangeRates).map((currency) => (
-            <MenuItem key={currency} value={currency}>
-              {currency}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <Box
-        sx={{
-          p: 2,
-          mt: 3,
-          textAlign: 'center',
-          background: 'linear-gradient(135deg, #bdd7ff, #c3cfe2)',
-          borderRadius: 2,
-        }}
-      >
-        <Typography component="h2" sx={{ color: '#404040', fontSize: '20px' }}>
-          Converted amount: {validConvertedAmount.toFixed(2)} {toCurrency}
-        </Typography>
-      </Box>
+      {Object.keys(rates).length === 0 ? (
+        <Box
+          sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100px' }}
+        >
+          <CircularProgress />
+        </Box>
+      ) : (
+        <>
+          <TextField
+            label="Amount"
+            type="number"
+            value={amount === 0 ? '' : amount}
+            onChange={handleAmountInput}
+            placeholder="Enter value..."
+            sx={{ marginBottom: 3, width: '100%' }}
+          />
+          <FormControl fullWidth sx={{ marginBottom: 2 }}>
+            <InputLabel>From</InputLabel>
+            <Select value={fromCurrency} onChange={handleFromCurrencySelect} label="From">
+              {Object.keys(rates).map((currency) => (
+                <MenuItem key={currency} value={currency}>
+                  {currency}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl fullWidth sx={{ marginBottom: 2 }}>
+            <InputLabel>To</InputLabel>
+            <Select value={toCurrency} onChange={handleToCurrencySelect} label="To">
+              {Object.keys(rates).map((currency) => (
+                <MenuItem key={currency} value={currency}>
+                  {currency}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <Box
+            sx={{
+              p: 2,
+              mt: 3,
+              textAlign: 'center',
+              background: 'linear-gradient(135deg, #bdd7ff, #c3cfe2)',
+              borderRadius: 2,
+            }}
+          >
+            <Typography component="h2" sx={{ color: '#404040', fontSize: '20px' }}>
+              Converted amount: {validConvertedAmount.toFixed(2)} {toCurrency}
+            </Typography>
+          </Box>
+        </>
+      )}
     </Box>
   );
 };
